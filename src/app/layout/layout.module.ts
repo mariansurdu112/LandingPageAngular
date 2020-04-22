87afd43ce5bba283dcebc7913973598a7512c21f
Marian <marian.surdu@blue-air.ro>
2020-04-22 22:47:21 +0300

    new structure

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,12 +6,19 @@ import { LayoutComponent } from './layout.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavbarComponent, LayoutComponent, HomeComponent, FooterComponent],
+  declarations: [HeaderComponent, NavbarComponent, LayoutComponent, HomeComponent,
+    FooterComponent, AboutComponent, ClientsComponent, ContactComponent, PortfolioComponent, ServicesComponent, TeamComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
-   ]
+  ]
 })
 export class LayoutModule { }

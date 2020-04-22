33323f0c8f66ac0fb23bc78fae85bf3d2996eaf8
Marian <marian.surdu@blue-air.ro>
2020-04-22 20:01:26 +0300

    structure

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutComponent } from './layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavbarComponent, LayoutComponent],
   imports: [
     CommonModule
   ]

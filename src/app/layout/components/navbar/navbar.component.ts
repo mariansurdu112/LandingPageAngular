86cb57ca7f08c907007e3bb2f05009e47ae7af65
Marian <marian.surdu@blue-air.ro>
2020-04-27 23:20:48 +0300

    move navbar into component

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  public loadScript() {
+    const body = document.body as HTMLDivElement;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = 'assets/js/agency.js';
+    script.async = true;
+    script.defer = true;
+    body.appendChild(script);
+  }
 
   ngOnInit(): void {
+    this.loadScript();
   }
 
 }

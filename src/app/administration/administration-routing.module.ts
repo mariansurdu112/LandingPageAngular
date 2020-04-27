366e3606f7ce11bb6626260705e133a8ba44f4bf
Marian <marian.surdu@blue-air.ro>
2020-04-28 00:05:30 +0300

    admin login component && add bootstrap

--- src/app/administration/administration-routing.module.ts
+++ src/app/administration/administration-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdministrationModule } from './administration.module';
 import { AdministrationComponent } from './administration.component';
+import { LoginAdminComponent } from './components/login/login.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 
 const routes: Routes = [
-  { path: '', component: AdministrationComponent }
+  {
+    path: '', component: AdministrationComponent,
+    children: [
+      { path: 'login', component: LoginAdminComponent},
+      { path: 'dashboard', component: AdminDashboardComponent }
+    ]
+  },
+
 ];
 
 @NgModule({
--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdministrationRoutingModule } from './administration-routing.module';
 import { AdministrationComponent } from './administration.component';
+import { LoginAdminComponent } from './components/login/login.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 
 @NgModule({
-  declarations: [AdministrationComponent],
+  declarations: [AdministrationComponent, LoginAdminComponent, AdminDashboardComponent],
   imports: [
     CommonModule,
     AdministrationRoutingModule
